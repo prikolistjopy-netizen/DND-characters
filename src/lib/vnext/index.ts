2e93d646070fe985e872d4fd3b153e7563a7bd16
prikolistjopy-netizen <prikolistjopy@gmail.com>
2026-06-29 21:37:47 +0300

    Export Diceborn semantic vNext modules

---
+++ src/lib/vnext/index.ts
@@ -0,0 +1,7 @@
+export * from './contracts';
+export * from './resolver';
+export * from './ruleEngine';
+export * from './resolverWithRules';
+export * from './resolverControlled';
+export * from './semanticCard';
+export * from './coverage';
